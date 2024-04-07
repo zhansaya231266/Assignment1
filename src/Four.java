8e4dec8dfc82a6835ae9746f685ea31469da4185
User <231266@astanait.edu.kz>
2024-04-07 20:06:12 +0500

    Assignment1

--- src/Four.java
+++ src/Four.java
@@ -1,2 +1,15 @@
 public class Four {
+    public static int Four(int n){
+        if(n==0){ // checking if n is 0. 0!=1
+            return 1;
+        } else{
+            return n*Four(n-1); //returning value of factorial
+        }
+    }
+
+    public static void main(String[] args){
+        int n = 5;
+        int result = Four(n);
+        System.out.println(result);
+    }
 }
