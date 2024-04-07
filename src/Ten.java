21a52d0f84e3a8720dbabc0861d7ae93c0995512
User <231266@astanait.edu.kz>
2024-04-07 22:09:54 +0500

    Assignment1

--- src/Ten.java
+++ src/Ten.java
@@ -1,9 +1,9 @@
 public class Ten {
     public static int Ten(int a, int b){
         if(b == 0){
-            return a;
+            return a; // b=0: GCD(a, b) = a
         } else{
-            return Ten(b, a%b);
+            return Ten(b, a%b); //Euclidean Algorithm
         }
     }
     public static void main(String[] args){
