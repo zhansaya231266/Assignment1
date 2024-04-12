c4b3edb78db31bb95eec8cb2b8a71cc4792bffb9
User <231266@astanait.edu.kz>
2024-04-12 09:44:44 +0500

    Assignment1

--- src/New.java
+++ src/New.java
@@ -1,3 +1,18 @@
 public class New {
-    public static 
+    public static int New (String soz, String bilmim) {
+        int count = 0;
+        int index = soz.indexOf(bilmim);
+        int i = 1;
+        while (i < index) {
+            count++;
+        }
+        return New(soz, bilmim);
+    }
+
+    public static void main(String[] args) {
+        String soz = "Database";
+        String bilmim = "a";
+        int res = New(soz, bilmim);
+        System.out.println(res);
+    }
 }
